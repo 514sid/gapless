7038aaadcff178109e25772594e00747b47bbb16
514sid <iam@514sid.com>
2026-05-13 00:09:30 +0400

    Add Maven Publish Plugin and Update Publishing Workflow
    - Added the Maven Publish plugin to build.gradle.kts for improved artifact management.
    - Updated the GitHub Actions workflow to generate a local staging bundle and upload it to the Sonatype Central Portal.
    - Adjusted the publishing configuration to use the new Central Portal API for artifact uploads.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -90,16 +90,8 @@ afterEvaluate {
                 }
             }
             maven {
-                name = "OSSRH"
-                url  = if (version.toString().endsWith("SNAPSHOT")) {
-                    uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-                } else {
-                    uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                }
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
+                name = "CentralPortalBundle"
+                url  = uri(layout.buildDirectory.dir("central-bundle").get().asFile)
             }
         }
     }
